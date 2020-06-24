725467e581ba1332c21426c79a6432479b78a71a
Jingting <fengshahuang@gmail.com>
2020-06-23 23:28:33 -0700

    Updates

---
+++ precache-manifest.5c8f51002373d3e96b5dd8be2eb045e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f091d88e4fec5216c2cefdb203c2744",
+    "url": "/shopping-cart-demo/index.html"
+  },
+  {
+    "revision": "8859b8c866dc379707b0",
+    "url": "/shopping-cart-demo/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "312c8b484dde77b8b7a4",
+    "url": "/shopping-cart-demo/static/css/main.31b92c50.chunk.css"
+  },
+  {
+    "revision": "8859b8c866dc379707b0",
+    "url": "/shopping-cart-demo/static/js/2.6f559c5b.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/shopping-cart-demo/static/js/2.6f559c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312c8b484dde77b8b7a4",
+    "url": "/shopping-cart-demo/static/js/main.ce32e7ed.chunk.js"
+  },
+  {
+    "revision": "cb5a62ab9cdde9133fbf",
+    "url": "/shopping-cart-demo/static/js/runtime-main.cd7eca7d.js"
+  },
+  {
+    "revision": "917a7dfbe9b2510a1b046c1dec4bc414",
+    "url": "/shopping-cart-demo/static/media/story_28_lrg.917a7dfb.jpg"
+  }
+]);
